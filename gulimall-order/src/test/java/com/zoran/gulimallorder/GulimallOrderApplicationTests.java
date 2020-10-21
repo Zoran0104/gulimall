b45c93eab211d2b5d5e0369a6b5f714c21228caf
zoran <zwyliufeng@outlook.com>
2020-10-21 19:55:48 +0800

    init project structure

---
+++ gulimall-order/src/test/java/com/zoran/gulimallorder/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoran.gulimallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gulimall-product/src/test/java/com/zoran/gulimallproduct/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoran.gulimallproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
