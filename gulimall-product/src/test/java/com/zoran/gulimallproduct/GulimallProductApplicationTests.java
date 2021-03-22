fb4eebfed85fa127b09ced83c063eaa93b8e1660
zoran <zwyliufeng@outlook.com>
2021-03-22 23:00:34 +0800

    引入aliyun-oss模块并解决依赖问题（版本一定要对）

--- gulimall-product/src/test/java/com/zoran/gulimallproduct/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/zoran/gulimallproduct/GulimallProductApplicationTests.java
@@ -1,19 +1,32 @@
 package com.zoran.gulimallproduct;
 
+import com.aliyun.oss.OSSClient;
 import com.zoran.gulimallproduct.service.CategoryService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
 	@Resource
-	private CategoryService categoryService;
+	private OSSClient ossClient;
+
+	/**
+	 * 用于测试OSS
+	 */
 	@Test
-	void contextLoads() {
-		System.out.println(categoryService.count());
+	public void testOSS() throws FileNotFoundException {
+		InputStream stream = new FileInputStream("/Users/zorantaylor/Downloads/2560x1600-4569418-white-hair-red-eyes-tokyo-ghoul-kaneki-ken-anime-boys-mask-anime.jpg");
+		System.out.println("hello");
+		ossClient.putObject("gulimall-zoran", "neki-ken-anime-boys-mask-anime.jpg", stream);
+		ossClient.shutdown();
+		System.out.println("success");
 	}
 
 }
