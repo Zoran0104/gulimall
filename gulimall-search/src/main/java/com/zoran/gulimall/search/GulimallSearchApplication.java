ba074cc26b67abd132c2aa68794a075abb357bf2
zoran <zwyliufeng@outlook.com>
2021-04-05 01:35:32 +0800

    配置调整，统一将web容器改为undertow

--- gulimall-search/src/main/java/com/zoran/gulimall/search/GulimallSearchApplication.java
+++ gulimall-search/src/main/java/com/zoran/gulimall/search/GulimallSearchApplication.java
@@ -1,9 +1,11 @@
 package com.zoran.gulimall.search;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
 public class GulimallSearchApplication {
     public static void main(String[] args) {
         SpringApplication.run(GulimallSearchApplication.class, args);
